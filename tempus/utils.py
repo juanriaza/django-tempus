3cf95e62d5b08da4c83f5ca6628bd0a6dc430920
Juan Riaza <juanriaza@gmail.com>
2013-04-27 11:08:02 +0200

    remove unnecesary param and pep8

--- tempus/utils.py
+++ tempus/utils.py
@@ -2,10 +2,12 @@ from django.core import signing
 
 
 def tempus_dumps(data, salt='tempus', compress=True, *args, **kwargs):
-    encrypted_data = signing.dumps(data, salt=salt, compress=compress, *args, **kwargs)
+    encrypted_data = signing.dumps(
+        data, salt=salt, compress=compress, *args, **kwargs)
     return encrypted_data
 
 
-def tempus_loads(encrypted_data, salt='tempus', max_age=None, compress=True, *args, **kwargs):
-    data = signing.loads(encrypted_data, salt=salt, max_age=max_age, *args, **kwargs)
+def tempus_loads(encrypted_data, salt='tempus', max_age=None, *args, **kwargs):
+    data = signing.loads(
+        encrypted_data, salt=salt, max_age=max_age, *args, **kwargs)
     return data
