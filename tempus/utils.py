6fb1231df2e5cc19a36938de098104b48d33b878
Juan Riaza <juanriaza@gmail.com>
2013-04-27 11:06:24 +0200

    remove bad compression handling

--- tempus/utils.py
+++ tempus/utils.py
@@ -3,13 +3,9 @@ from django.core import signing
 
 def tempus_dumps(data, salt='tempus', compress=True, *args, **kwargs):
     encrypted_data = signing.dumps(data, salt=salt, compress=compress, *args, **kwargs)
-    if compress:
-        return encrypted_data[1:]
     return encrypted_data
 
 
 def tempus_loads(encrypted_data, salt='tempus', max_age=None, compress=True, *args, **kwargs):
-    if compress:
-        encrypted_data = '.%s' % encrypted_data
     data = signing.loads(encrypted_data, salt=salt, max_age=max_age, *args, **kwargs)
     return data
